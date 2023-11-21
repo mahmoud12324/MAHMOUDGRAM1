1b1e3b84aa36a897bf90714ea8a99e8f9d4f63b4
mahmoud12324 <mahmoud.shhadi1234@gmail.com>
2023-11-22 00:19:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mahmoudgram/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.mahmoudgram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etEmail,etPassword;
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       conectComponents();
+    }
+
+    private void conectComponents() {
+        etEmail= findViewById(R.id.etEmailMain);
+                etPassword=findViewById(R.id.etPasswordMain);
+    }
+
+    public void signup(View view) {
+        String password,email;
+        email=etEmail.getText().toString();
+        password=etPassword.getText().toString();
+if(email.trim().isEmpty()||password.trim().isEmpty())
+{
+    Toast.makeText(this, "some feilds are missing!", Toast.LENGTH_SHORT).show();
+    return;
+}
+
+
+
+    }
+}
